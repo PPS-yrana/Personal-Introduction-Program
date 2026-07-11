c42021eaee0e59c867e484b203a3ddba1987efde
PPS-yrana <yashhrana130@gmail.com>
2026-07-12 00:49:49 +0530

    Initial commit

---
+++ personal_intro.py
@@ -0,0 +1,16 @@
+# Personal Introduction Program
+
+# User input for name
+user_name = input('What is your name? ') 
+
+# User input for age
+user_age = input('How old are you? ') 
+
+# User input for hobby
+user_hobby = input('What is your favorite hobby? ') # User input for hobby
+
+# Welcome Message
+print(f'🎉 Welcome {user_name}! 🎉') 
+
+# f-string to display user information
+print(f'You are {user_age} years old and love {user_hobby}.')
